ac75be7ad65ebf73afc0e2e7da375a18271d2aa3
shmulikkaufman <shmulikmail@gmail.com>
2023-12-11 11:00:15 +0200

    update

--- AKS/userdata-install_jumper_tools.sh
+++ AKS/userdata-install_jumper_tools.sh
@@ -17,14 +17,19 @@ rm ./get_helm.sh
 echo "Installing azure cli ..."
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
-# Install vim, wget, unzip, git, maven, openjdk11
+# Install vim, wget, unzip, git, maven, openjdk11, k9s
 echo "Installing vim, wget, unzip, git, maven, openjdk11"
 if [[ $(cat /etc/*-release |grep -i "ubuntu\|debian" |wc -l) > 0 ]];then 
   sudo apt-get install -y  openjdk-11-jdk wget unzip maven
+  sudo snap install k9s --devmode
+
 elif [[ $(cat /etc/*-release |grep -i "ubuntu\|debian" |wc -l) > 0 ]];then
   sudo yum install -y vim wget unzip maven java-11-openjdk-devel
+  curl -sS https://webi.sh/k9s | sh
+
 elif [[ $(uname) == "Darwin" ]];then
-  brew brew install vim wget unzip maven openjdk@11
+  brew brew install vim wget unzip maven openjdk@11 k9s
+
 else
   echo "Unsupported Linux distribution, please install vim, wget, unzip, git, maven, openjdk11 manually."
 fi
